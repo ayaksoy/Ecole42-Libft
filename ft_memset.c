a89099599f37f817cc5c83eebf5500de29d67139
ayaksoy <yasir.aksoy@outlook.com>
2024-06-19 16:23:16 +0300

    memset

---
+++ ft_memset.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaksoy <aaksoy@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 20:16:58 by aaksoy            #+#    #+#             */
+/*   Updated: 2023/12/08 00:16:51 by aaksoy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	i = 0;
+	str = (unsigned char *)b;
+	while (i < len)
+		str[i++] = c;
+	return (b);
+}
