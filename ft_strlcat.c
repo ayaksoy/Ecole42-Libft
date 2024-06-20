383654ec5fd00282026f159dc58e1924122dfe88
ayaksoy <yasir.aksoy@outlook.com>
2024-06-20 19:34:04 +0300

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strlcat.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaksoy <aaksoy@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 20:22:49 by aaksoy            #+#    #+#             */
+/*   Updated: 2023/11/15 20:23:29 by aaksoy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	while (dst[j] && j < size)
+		j++;
+	i = j;
+	while (src[j - i] && j + 1 < size)
+	{
+		dst[j] = src[j - i];
+		j++;
+	}
+	if (i < size)
+		dst[j] = '\0';
+	return (i + ft_strlen(src));
+}
