cc86683984d66fd0e0827db26b7f867488e27c84
ayaksoy <yasir.aksoy@outlook.com>
2024-06-20 19:38:21 +0300

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaksoy <aaksoy@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/26 17:00:03 by aaksoy            #+#    #+#             */
+/*   Updated: 2023/11/26 17:00:29 by aaksoy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*ret;
+	int		i;
+
+	i = 0;
+	if (!s || !f)
+		return (0);
+	ret = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (ret == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		ret[i] = f(i, s[i]);
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
