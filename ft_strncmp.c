257e018b20f2ddfac21fa4eeb455d3c07b4d867a
ayaksoy <yasir.aksoy@outlook.com>
2024-06-23 11:15:00 +0300

    strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaksoy <aaksoy@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 20:20:43 by aaksoy            #+#    #+#             */
+/*   Updated: 2023/11/15 20:20:45 by aaksoy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
+		}
+		i++;
+	}
+	return (0);
+}
